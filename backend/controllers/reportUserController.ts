38a3a4b500ddeb63ea12ab00bde5b8a7acf0f06e
Jhonny <jhonnypv14@gmail.com>
2022-06-19 16:55:40 -0600

    Operator profile ready

--- backend/controllers/reportUserController.ts
+++ backend/controllers/reportUserController.ts
@@ -1,14 +1,14 @@
-import { /*ReservationRep,*/ ParkingRep, /*UserRep*/ } from '../repository';
+import { ReservationRep, ParkingRep, /*UserRep*/ } from '../repository';
 
 export class ReportUserController {
 
     private static instance: ReportUserController;
-    //private reservationRep: ReservationRep;
+    private reservationRep: ReservationRep;
     private parkingRep: ParkingRep;
     //private userRep: UserRep;
 
     private constructor() {
-      //  this.reservationRep = new ReservationRep();
+        this.reservationRep = new ReservationRep();
         this.parkingRep = new ParkingRep();
         //this.userRep = new UserRep();
     }
@@ -40,23 +40,84 @@ export class ReportUserController {
             }
 
             //getparking and check if is correct
-            const parking = await this.parkingRep.getParkingById(parkinglotId);
+            var parking = await this.parkingRep.getParkingById(parkinglotId);
             if (!parking) {
                 rj("No user or parkinglot found"); // reject
                 return;
             }
 
-            //Set the correct format for the schedule and contracts' limits
+            //Return parking details without filter
+            if (start != '0' && end != '0') {
+
+                var s = new Date(start);
+                var e = new Date(end);
+                console.log("HERE");
+                await this.consultOccupationStandarUserAndVisitor(parking, s, e);
+                await this.consultOccupationOfficialVehicles(parking);
+                await this.consultOccupationLeaderships(parking, s);
+                await this.consultOccupationDisabledSpaces(parking, s, e);
+            }
+
+            //Set the correct format for the schedule limits
             parking.schedule.startHour = (new Date(parking.schedule.startHour.seconds * 1000)).toLocaleTimeString();
             parking.schedule.endHour = (new Date(parking.schedule.endHour.seconds * 1000)).toLocaleTimeString();
 
-
-            //Return parking details without filter
-            /*if (start == '0') 
-                rs(parking);*/
-            
             rs(parking);
         });
     }
 
+    async consultOccupationStandarUserAndVisitor(parking: any, start: Date, end: Date) {
+
+        var reservations = await this.reservationRep.getReservationsByTypeByParking('Funcionario', parking.id);
+        var count = 0;
+
+        reservations.forEach(function (r: any) {
+            if (start.getTime() < r.end.seconds * 1000 && r.start.seconds * 1000 < end.getTime())
+                count++;
+        });
+
+        var reservations2 = await this.reservationRep.getReservationsByTypeByParking('Visitante', parking.id);
+        reservations2.forEach(function (r: any) {
+            if (start.getTime() < r.end.seconds * 1000 && r.start.seconds * 1000 < end.getTime())
+                count++;
+        });
+
+        if (parking.othersSpaces < count) parking.busyOthersSpaces = parking.othersSpaces;
+        else parking.busyOthersSpaces = count;
+    }
+
+    async consultOccupationOfficialVehicles(parking: any) {
+        var reservations = await this.reservationRep.getReservationsByTypeByParking('Vehiculo Oficial', parking.id);
+        parking.busyVehiclesSpaces = reservations.length;
+    }
+
+
+    async consultOccupationLeaderships(parking: any, start: Date) {
+        var reservations = await this.reservationRep.getReservationsByTypeByParking('Jefatura', parking.id);
+        var count = 0;
+
+        reservations.forEach(function (r: any) {
+            if (start.toDateString() == (new Date(r.date.seconds * 1000)).toDateString())
+                count++;
+        });
+        parking.busyAdministrativeSpaces = count;
+    }
+
+    async consultOccupationDisabledSpaces(parking: any, start: Date, end: Date) {
+
+        //Consult the reservation for Discapacitado type and parking
+        var reservations = await this.reservationRep.getReservationsByTypeByParking('Discapacitado', parking.id);
+        var count = 0;
+
+        reservations.forEach(function (r: any) {
+            if (r.date && (new Date(r.date.seconds * 1000)).toDateString() == start.toDateString())
+                count++;
+
+            else if (!r.date && start.getTime() < r.end.seconds * 1000 && r.start.seconds * 1000 < end.getTime())
+                count++;
+        });
+
+        if (parking.disabledSpaces < count) parking.busyDisabledSpaces = parking.disabledSpaces;
+        else parking.busyDisabledSpaces = count;
+    }
 }
